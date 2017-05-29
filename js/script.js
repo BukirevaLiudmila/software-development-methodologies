8c9d668af48ea1f63eb51d19c0dfb4333060edc6
Liudmila <226585@niuitmo.ru>
2017-05-29 15:07:42 +0300

    new solution

--- js/script.js
+++ js/script.js
@@ -1,96 +1,40 @@
-function showResult(classResult, textResult) {
-    var reactionOnSending = document.querySelector('#reactionOnSending');
-    reactionOnSending.classList.add(classResult);
-    reactionOnSending.appendChild(document.createTextNode(textResult));
-    reactionOnSending.style.visibility = 'visible';
-    setTimeout(function () {
-        reactionOnSending.style.visibility = 'hidden';
-        reactionOnSending.classList.remove(classResult);
-        reactionOnSending.removeChild(reactionOnSending.firstChild);
-    }, 3000);
-}
-
-function sendMail() {
-    var data = {
-        name: document.querySelector('input[name="name"]').value,
-        email: document.querySelector('input[name="email"]').value,
-        message: document.querySelector('textarea[name="message"]').value
-    };
-    fetch('/send-mail', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then(function (x) {
-            return x.json();
-        })
-        .then(function (x) {
-            if (x.classResult == 'sucsess-text') {
-                document.querySelector('input[name="name"]').value = '';
-                document.querySelector('input[name="email"]').value = '';
-                document.querySelector('textarea[name="message"]').value = '';
-            }
-            showResult(x.classResult, x.textResult);
-            console.log(x);
-        });
-    return false;
-}
+// Установка значения по умолчанию, первая картинка
+let lastImg = document.querySelector('.image-block img');
 
 function isVisible(elem) {
-    var coords = elem.getBoundingClientRect();
-    var windowHeight = document.documentElement.clientHeight;
-    var topVisible = coords.top > 0 && coords.top < windowHeight;
-    var bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+    const coords = elem.getBoundingClientRect();
+    const windowHeight = document.documentElement.clientHeight;
+    const topVisible = coords.top > 0 && coords.top < windowHeight;
+    const bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
     return topVisible || bottomVisible;
 }
 
-function showVisible() {
-    var progressBars = document.getElementsByClassName('progress-bar');
-    for (var i = 0; i < progressBars.length; i++) {
-        var bar = progressBars[i];
-        var showed = bar.getAttribute('data-showed');
-        if (showed == 'true') continue;
-        if (isVisible(bar)) {
-            bar.setAttribute('style', 'width: ' + bar.getAttribute('aria-valuenow') + '%');
-            bar.setAttribute('data-showed', 'true');
+// Функция для установки фоновой картинки
+function setBackgroindImg() {
+    // Выбор всех интересующих нас картинок
+    const imgs = document.querySelectorAll('.image-block img');
+    // Цикл по всем картинкам
+    for (let i = 0; i < imgs.length; i++) {
+        const img = imgs[i];
+        // Проверка: видна ли на экране картинка
+        if (isVisible(img)) {
+            lastImg = img;
         }
     }
-    var screens = document.querySelectorAll('.screen');
-    var coordsMiddle = [];
-    screens.forEach(function (elem) {
-        var coords = elem.getBoundingClientRect();
-        var middleScreen = (coords.top + coords.bottom) / 2 - document.documentElement.clientHeight / 2;
-        coordsMiddle.push({
-            coords: middleScreen,
-            elem: elem.getAttribute('id')
-        });
-    });
-    var activeScreen = coordsMiddle[0];
-    coordsMiddle.forEach(function (elem) {
-        if (Math.abs(elem.coords) < Math.abs(activeScreen.coords)) {
-            activeScreen = elem;
-        }
-    });
-    var imgs = document.querySelectorAll('.circle > img');
-    imgs.forEach((el) => {
-        var attr = el.getAttribute('data-scroll-to');
-        if (attr == activeScreen.elem) {
-            el.setAttribute('src', './images/circle-active.png');
-        } else {
-            el.setAttribute('src', './images/circle.png');
-        }
-    });
+    const background = document.querySelector('.background');
+    // Установка фона
+    background.setAttribute('style', `background-image: url("${lastImg.getAttribute('src')}");`);
 }
 
-window.onscroll = showVisible;
-showVisible();
+//
+window.onscroll = setBackgroindImg;
+setBackgroindImg();
 
+/*
 var edgeOffset = 0; // px
 zenscroll.setup(null, edgeOffset);
 
-/* переключение экранов (меню-кружочки) */
+/!* переключение экранов (меню-кружочки) *!/
 function scroll(e) {
     var to = e.target.getAttribute('data-scroll-to');
     if (to == null) {      // если нажали на стрелку на первом экране
@@ -104,9 +48,8 @@ function scroll(e) {
 document.querySelectorAll('.circle').forEach(function (el) {
     el.addEventListener('click', scroll);
 });
+*/
 
 /* стрелка на первом экране */
-var arrow = document.querySelector('.hvr-hang');
-arrow.addEventListener('click', scroll);
-
-slidr.create('slidr-id').start();
+/*var arrow = document.querySelector('.hvr-hang');
+arrow.addEventListener('click', scroll);*/
